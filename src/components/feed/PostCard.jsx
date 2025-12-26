334110a0dc9e9e5794983c97b37e168ff72227a2
Robert Davis <robertdavisiv87@gmail.com>
2025-12-26 23:46:24 +0000

    File changes

--- src/components/feed/PostCard.jsx
+++ src/components/feed/PostCard.jsx
@@ -177,7 +177,7 @@ export default function PostCard({ post, currentUser, onLike, onTip }) {
               <MoreHorizontal className="w-5 h-5" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuContent align="end" className="w-48 bg-gradient-to-b from-purple-50 to-pink-50 border-purple-200">
             {isOwnPost ? (
               <>
                 <DropdownMenuItem onClick={handleDeletePost} disabled={isDeleting} className="text-red-600 focus:text-red-600">
